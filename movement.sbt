0f90eb075af83191db3f57a663515bde58bcf5fb
ctrenner <ctrenner@gmx.at>
2012-03-01 21:08:48 +0100

    initial commit

---
+++ movement.sbt
@@ -0,0 +1,10 @@
+name := "randomMovement"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
+
+libraryDependencies ++= Seq(
+)
+
+
---
+++ src/main/scala/at/desrud/randmove/Main.scala
@@ -0,0 +1,9 @@
+package at.desrud.randmove
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("move")
+  }
+
+}
